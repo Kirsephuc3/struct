915a2e2b32dfe9c7f9954a91f81178dd9cc0aca0
Kirsephuc3 <101470532+Kirsephuc3@users.noreply.github.com>
2022-04-15 16:22:38 +0700

    Add files via upload

--- 11.cpp
+++ 11.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include "gpg.h"
-using namespace std;
-int main() {
-	GPG g;
-	cout << g.geth() << " : " << g.getm() << " : " << g.gets() << endl;		
-	g.set(20, 10, 3);
-	cout << g.geth() << " : " << g.getm() << " : " << g.gets() << endl;
-}
+//#include <iostream>
+//#include "gpg.h"
+//using namespace std;
+//int main() {
+//	GPG g;
+//	cout << g.geth() << " : " << g.getm() << " : " << g.gets() << endl;		
+//	g.set(20, 10, 3);
+//	cout << g.geth() << " : " << g.getm() << " : " << g.gets() << endl;
+//}
